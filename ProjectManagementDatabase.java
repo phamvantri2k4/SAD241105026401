ad4f33f7ab52af90632695b9914e6c10a974c0ec
phamvantri2k4 <140030277+phamvantri2k4@users.noreply.github.com>
2024-11-26 10:29:40 +0700

    Delete ProjectManagementDatabase.java

--- ProjectManagementDatabase.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-import java.util.List;
-
-public class ProjectManagementDatabase {
-    // Simulate retrieving charge numbers from the database
-    public List<String> getChargeCodes() {
-        return Arrays.asList("MS01", "MS02", "MS03", "MS04");
-    }
-}
