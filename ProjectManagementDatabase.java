daa37b612c6da3c63435d64819a06f65b3a2e14e
phamvantri2k4 <140030277+phamvantri2k4@users.noreply.github.com>
2024-11-26 10:23:39 +0700

    Lab2
    Lab2 SAD

---
+++ ProjectManagementDatabase.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ProjectManagementDatabase {
+    // Simulate retrieving charge numbers from the database
+    public List<String> getChargeCodes() {
+        return Arrays.asList("MS01", "MS02", "MS03", "MS04");
+    }
+}
